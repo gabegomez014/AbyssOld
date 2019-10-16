eb595c16fad34414ee49e25b3814efcce59a07c8
Gabriel Gomez <gabrielgomez@Gabriels-MBP.lan1>
2019-10-16 16:41:20 -0400

    Added a gitignore

--- Assets/Scripts/Ability.cs
+++ Assets/Scripts/Ability.cs
@@ -8,8 +8,9 @@ public abstract class Ability : ScriptableObject
     public Sprite aSprite;                          // The sprite for the cooldown
     public AudioClip aSound;                        // The sound for the ability
     public float aBaseCoolDown = 1f;                // The ability's cooldown
-    public float lifeTime = 1f;                     // How long the ability can live
+    public float aLifetime = 1f;                     // How long the ability can live
     public GameObject aParticles;                   // The particle systems prefab for the ability
+    public Transform spawn;
 
     public abstract void Initialize(GameObject obj);
     public abstract void TriggerAbility(int abilityIndex);
