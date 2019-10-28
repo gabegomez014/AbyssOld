0a88b548e0967462a5ce1c137293d15b299cfe87
Gabriel Gomez <gabrielgomez@Gabriels-MBP.lan1>
2019-10-28 04:41:11 -0400

    Brought in Character model and applied animations; need to tweak movement a bit to take out jitters

---
+++ Assets/Scripts/PlayerAnimator.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerAnimator : MonoBehaviour
+{
+    const float movementSmoothTime = 0.1f;
+
+    private const float EPSILON = 0.2f;
+
+    Animator animator;
+    Rigidbody rb;
+    PlayerController pc;                // Need this to get the speed
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        animator = GetComponentInChildren<Animator>();
+        pc = GetComponent<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveDirection = rb.velocity.x;
+
+        if (moveDirection < 0)
+        {
+            moveDirection = 0;
+        }
+
+        else if (System.Math.Abs(moveDirection) < EPSILON)
+        {
+            moveDirection = 0.5f;
+        }
+
+        else if (moveDirection > 0)
+        {
+            moveDirection = 1;
+        }
+        animator.SetFloat("movementPercent", moveDirection, movementSmoothTime, Time.deltaTime);
+
+    }
+}
--- Assets/Scripts/PlayerMover.cs
+++ Assets/Scripts/PlayerMover.cs
@@ -5,9 +5,19 @@ using UnityEngine.EventSystems;
 
 public class PlayerMover : MonoBehaviour
 {
+    private const float EPSILON = 0.2f;
+
     [SerializeField]
     private float timeSlowDown = 0.1f;      // The point where time gets slowed down to
     private bool teleporting = false;       // True when player hits the teleport button, false otherwise
+    Rigidbody rb;
+
+    [HideInInspector] public float speed = 5f;         // This is how fast a character moves. This will be replaced by character stat once the stats system gets implemented
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();    
+    }
 
     // Update is called once per frame
     void Update()
@@ -23,16 +33,7 @@ public class PlayerMover : MonoBehaviour
         if (Input.touchCount > 0)
         {
             Touch touch = Input.GetTouch(0);
-
-            if (teleporting)
-            {
-                TouchRegistered(touch);
-            }
-
-            else
-            {
-                TouchRegistered(touch);
-            }
+            TouchRegistered(touch);
         }
     }
 
@@ -59,12 +60,33 @@ public class PlayerMover : MonoBehaviour
             teleporting = false;
         }
 
-        else if (touch.phase == TouchPhase.Moved && !teleporting)
+        else if (touch.phase == TouchPhase.Stationary || touch.phase == TouchPhase.Moved && !teleporting)
         {
             Vector3 touchPosition = Camera.main.ScreenToWorldPoint(touch.position);
             touchPosition.y = 0;
             touchPosition.z = transform.position.z;
-            transform.position = touchPosition;
+
+            if (System.Math.Abs(transform.position.x - touchPosition.x) > EPSILON)
+            {
+                print(System.Math.Abs(transform.position.x - touchPosition.x));
+                var direction = transform.position.x - touchPosition.x;         // If positive, move to the lef. If negative, move to the right
+
+                if (direction > 0)
+                {
+                    rb.velocity = new Vector3(-speed,0,0);          // Move Left
+                }
+
+                else if (direction < 0)
+                {
+                    rb.velocity = new Vector3(speed, 0, 0);         // Move Right
+                }
+                
+            }
+        }
+
+        else if (touch.phase == TouchPhase.Ended)
+        {
+            rb.velocity = new Vector3(0,0,0);               // Stop movements when the finger is lifted
         }
     }
 }
